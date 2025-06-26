a3e0cc8b79b339002e3c9f718c1003bd6ceaa486
Shiva <luxdrive0360@gmail.com>
2025-06-26 16:02:49 +0530

    sign up form added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Navbar from './components/common/Navbar';
+import Footer from './components/common/Footer';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
-      <Navbar />
+    
+      <Dashboard />
   );
 }
 
